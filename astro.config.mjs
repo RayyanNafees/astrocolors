284352b33b9576762137942074efb0e440c52486
RayyanNafees <nafees.rayyan@gmail.com>
2023-07-22 12:41:08 +0530

    Tabs reduced to components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // astro.config.ts
-import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   integrations: [
