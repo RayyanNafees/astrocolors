2e38609246f4b1ed79ee812d361e8fab293e1771
Rayyan Nafees <59685062+RayyanNafees@users.noreply.github.com>
2024-02-22 10:38:05 +0000

    Updated Deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // astro.config.ts
 import UnoCSS from 'unocss/astro';
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [UnoCSS()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    edgeMiddleware: true
+  })
 });
